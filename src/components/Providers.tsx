447c9d2907e8f42186c4f272a89d224c5e4fdf05
Nithin <nithinkatkam504106@gmail.com>
2024-11-04 18:08:25 +0530

    provider added

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,6 +5,7 @@ import {
   darkTheme,
 } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
+import { http } from "wagmi";
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
@@ -15,12 +16,13 @@ import {
   sepolia,
   optimismSepolia,
   arbitrumSepolia,
+  baseSepolia,
 } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
-const config = getDefaultConfig({
-  appName: "Some App",
+export const config = getDefaultConfig({
+  appName: "Talent Link App",
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID || "",
   chains: [
     mainnet,
@@ -29,7 +31,13 @@ const config = getDefaultConfig({
     sepolia,
     optimismSepolia,
     arbitrumSepolia,
+    baseSepolia,
   ],
+  transports: {
+    [baseSepolia.id]: http(
+      "https://base-sepolia.g.alchemy.com/v2/POcytJtZjkzStgaMseE9BxpHexaC4Tfj"
+    ),
+  },
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
