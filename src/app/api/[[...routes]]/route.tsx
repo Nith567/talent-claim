635f29556bfc7ed17fbf5b86031774d962525c62
Nithin <nithinkatkam504106@gmail.com>
2024-11-04 18:41:00 +0530

    wallet neynar

--- src/app/api/[[...routes]]/route.tsx
+++ src/app/api/[[...routes]]/route.tsx
@@ -37,12 +37,13 @@ app.frame("/", async (c) => {
     action: "/passport",
     image: (
       <div style={{ color: "white", display: "flex", fontSize: 60 }}>
-        Signature: {frameData?.address}
+        builderscore
       </div>
     ),
     intents: [
       <Button key="sign" action="/sign">
         sign {frameData?.address ?? "no address"}
+        {wallets ? wallets[0] || "no wallet" : "no wallet"}
       </Button>,
     ],
   });
